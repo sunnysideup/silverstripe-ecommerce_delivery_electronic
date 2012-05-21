e8b3f30edf864939d68ec4ba661d0578d8ca0abf
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-05-21 08:04:21 +0000

    API: first viable version
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_delivery_electronic/trunk@2650 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -8,15 +8,23 @@
 
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START ecommerce_delivery MODULE ----------------===================
+//===================---------------- START ecommerce_delivery_electronically MODULE ----------------===================
 //MUST SET
-//Order::add_modifier("PickUpOrDeliveryModifier"); // OR //Order::set_modifiers(array("PickUpOrDeliveryModifier"));
-//StoreAdmin::add_managed_model("PickUpOrDeliveryModifierOptions");
-//Object::add_extension('EcommerceCountry', 'PickUpOrDeliveryModifierOptionsCountry');
-//Object::add_extension('EcommerceRegion', 'PickUpOrDeliveryModifierOptionsRegion');
+//ElectronicDelivery_OrderLog::set_random_folder_name_character_count(12);
+//ElectronicDelivery_OrderLog::set_add_htaccess_file(true);
+//ElectronicDelivery_OrderLog::set_files_to_be_excluded(array());
+//ElectronicDelivery_OrderLog::set_permissions_on_folder("0755");
+//ElectronicDelivery_OrderLog::set_order_dir("downloads");
+//===================---------------- END ecommerce_delivery_electronically  MODULE ----------------===================
+/*
 
-//MAY SET
-//PickUpOrDeliveryModifier::set_form_header("Delivery Option (REQUIRED)");
+dont forget to add the following .htaccess file to your originating folders:
 
-//NOTE: add http://svn.gpmd.net/svn/open/multiselectfield/tags/0.2/ for nicer interface
-//===================---------------- END ecommerce_delivery  MODULE ----------------===================
+IndexIgnore *
+order allow,deny
+deny from all
+
+so that no-one can access the original files.
+
+
+*/

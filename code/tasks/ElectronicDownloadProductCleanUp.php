e5318a774af9c3c6e4f7ce55ac8142c39e818df1
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-11-22 10:26:07 +0000

    adding files
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_delivery_electronic/trunk@5243 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/tasks/ElectronicDownloadProductCleanUp.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+class ElectronicDownloadProductCleanUp extends BuildTask{
+
+	protected $title = "Remove expired downloads from the download folder.";
+
+	protected $description = "Removes all the expired downloads from the download folder.";
+
+	public function run($request) {
+		$items = ElectronicDelivery_OrderLog::get()->filter(array("Completed" => 0));
+		foreach($items as $item) {
+			if($item->IsExpired()) {
+				//a simple write will take care of all the deletion process...
+				$item->write();
+			}
+		}
+	}
+
+}

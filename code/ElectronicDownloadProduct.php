7a58fff125cedf4014b4bb2f0f3b49676dd540f4
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:59:44 +1300

    Converted to PSR-2

--- code/ElectronicDownloadProduct.php
+++ code/ElectronicDownloadProduct.php
@@ -1,66 +1,69 @@
 <?php
 
-class ElectronicDownloadProduct extends Product {
+class ElectronicDownloadProduct extends Product
+{
 
-	/**
-	 * Standard SS variable.
-	 */
-	private static $many_many = array(
-		'DownloadFiles' => 'File'
-	);
+    /**
+     * Standard SS variable.
+     */
+    private static $many_many = array(
+        'DownloadFiles' => 'File'
+    );
 
-	/**
-	 * Standard SS variable.
-	 */
-	private static $icon = 'ecommerce_delivery_electronic/images/icons/ElectronicDownloadProduct';
+    /**
+     * Standard SS variable.
+     */
+    private static $icon = 'ecommerce_delivery_electronic/images/icons/ElectronicDownloadProduct';
 
-	/**
-	 * Standard SS variable.
-	 * @var String
-	 */
-	private static $description = "A product can be downloaded.";
+    /**
+     * Standard SS variable.
+     * @var String
+     */
+    private static $description = "A product can be downloaded.";
 
-	/**
-	 * Standard SS variable.
-	 */
-	private static $singular_name = "Electronic Download Product";
-		function i18n_singular_name() { return _t("ElectronicDownloadProduct.SINGULAR_NAME", "Electronic Download Product");}
+    /**
+     * Standard SS variable.
+     */
+    private static $singular_name = "Electronic Download Product";
+    public function i18n_singular_name()
+    {
+        return _t("ElectronicDownloadProduct.SINGULAR_NAME", "Electronic Download Product");
+    }
 
-	/**
-	 * Standard SS variable.
-	 */
-	private static $plural_name = "Electronic Download Products";
-		function i18n_plural_name() { return _t("ElectronicDownloadProduct.PLURAL_NAME", "Electronic Download Products");}
+    /**
+     * Standard SS variable.
+     */
+    private static $plural_name = "Electronic Download Products";
+    public function i18n_plural_name()
+    {
+        return _t("ElectronicDownloadProduct.PLURAL_NAME", "Electronic Download Products");
+    }
 
 
-	/**
-	 * Standard SS Method
-	 */
-	function getCMSFields() {
-		$fields = parent::getCMSFields();
-		$fields->addFieldToTab('Root.Details', new UploadField('DownloadFiles', _t('ElectronicDownloadProduct.DOWNLOADFILES', 'Download Files')));
-		return $fields;
-	}
+    /**
+     * Standard SS Method
+     */
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab('Root.Details', new UploadField('DownloadFiles', _t('ElectronicDownloadProduct.DOWNLOADFILES', 'Download Files')));
+        return $fields;
+    }
 
-	/**
-	 * This is used when you add a product to your cart
-	 * if you set it to 1 then you can add 0.1 product to cart.
-	 * If you set it to -1 then you can add 10, 20, 30, etc.. products to cart.
-	 *
-	 * @return Int
-	 **/
-	function QuantityDecimals(){
-		return 0;
-	}
-
+    /**
+     * This is used when you add a product to your cart
+     * if you set it to 1 then you can add 0.1 product to cart.
+     * If you set it to -1 then you can add 10, 20, 30, etc.. products to cart.
+     *
+     * @return Int
+     **/
+    public function QuantityDecimals()
+    {
+        return 0;
+    }
 }
 
 
-class ElectronicDownloadProduct_Controller extends Product_Controller {
-
+class ElectronicDownloadProduct_Controller extends Product_Controller
+{
 }
-
-
-
-
-

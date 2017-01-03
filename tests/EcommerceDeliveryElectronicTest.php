2f5c09f265177a25d276f4026d8c764578c24e0f
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 14:05:22 +1300

    PATCH: module clean-up

---
+++ tests/EcommerceDeliveryElectronicTest.php
@@ -0,0 +1,13 @@
+<?php
+class EcommerceDeliveryElectronicTest extends SapphireTest {
+
+	protected $usesDatabase = false;
+
+	protected $requiredExtensions = array();
+
+	public function testMyMethod() {
+		$this->assertEquals(1, 1);
+	}
+
+}
+
